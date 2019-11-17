4b102bfde686b5da0d1f9cd6e16a8590a8bb97b6
EC2 Default User <ec2-user@ip-172-31-36-208.eu-west-1.compute.internal>
2019-11-17 11:26:08 +0000

    almost there cleaned up css alot cleaned sopme code and considering adding a bit more

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -2,84 +2,83 @@
 $(document).ready(function() {
 
     $('#aboutSec').toggle();
-    $('#notesSec').toggle();
+    $('#notesSec').show();
     $('#gamesSec').toggle();
     $('#linksSec').toggle();
     $('.fa-check-square').toggle();
+    $('#headerwelcome').show();
+    $('#headerabout').toggle();
+    $('#headernotes').toggle();
 
     //$('#item').toggle();
     //$('#item1').toggle();
     //$('#item2').toggle();
 
     $('#aboutNav').on('click', function() {
-        $('#aboutSec').toggle(1000);
-
+        $('#aboutSec').show(1000);
+        $('#notesSec').hide(1000);
+        $('#headerwelcome').hide();
+        $('#headerabout').show();
+        $('#headernotes').hide();
     });
 
     $('#notesNav').on('click', function() {
-        $('#notesSec').toggle(1000);
-
-    });
-
-    $('#gamesNav').on('click', function() {
-        $('#gamesSec').toggle(1000);
-
-    });
-
-    $('#linksNav').on('click', function() {
-        $('#menu3').toggle();
-        $('#linksSec').toggle(1000);
+        $('#notesSec').show(1000);
+        $('#aboutSec').hide(1000);
+        $('#headerwelcome').hide();
+        $('#headernotes').show();
+        $('#headerabout').hide();
 
     });
 
     $('#checkBox').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare').toggle();
+        $('#fabox').toggle();
     });
 
     $('#checkBox1').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare1').toggle();
+        $('#fabox1').toggle();
     });
 
     $('#checkBox2').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare2').toggle();
+        $('#fabox2').toggle();
     });
 
     $('#checkBox3').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare3').toggle();
+        $('#fabox3').toggle();
     });
 
     $('#checkBox4').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare4').toggle();
+        $('#fabox4').toggle();
     });
 
     $('#checkBox5').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare5').toggle();
+        $('#fabox5').toggle();
     });
 
     $('#checkBox6').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare6').toggle();
+        $('#fabox6').toggle();
     });
 
     $('#checkBox7').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare7').toggle();
+        $('#fabox7').toggle();
     });
 
     $('#checkBox8').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare8').toggle();
+        $('#fabox8').toggle();
     });
 
     $('#checkBox9').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
+        $('#fasquare9').toggle();
+        $('#fabox9').toggle();
     });
 
 
